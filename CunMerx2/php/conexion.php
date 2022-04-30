78697806feac7f998693010ebe871e51f46a2f03
CumMerx <103858235+CumMerx@users.noreply.github.com>
2022-04-30 08:35:29 -0500

    Add files via upload

---
+++ CunMerx2/php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+        
+        $conexion = pg_connect("host=localhost dbname=CunMerx user= postgres password=1234");
+       /*
+        if($conexion){
+            echo " se conecto";
+        }else{
+            echo "  no se conecto";
+        }
+        */
+?>        
+        
